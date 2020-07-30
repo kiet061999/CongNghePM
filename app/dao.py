ab6f096ae05acb7fe6a814d2998e9dc685e793f9
1751010067kiet@ou.edu.vn <1751010067kiet@ou.edu.vn>
2020-07-30 09:20:12 +0700

    init project

---
+++ app/dao.py
@@ -0,0 +1,16 @@
+from app import app
+import  json
+import os
+
+def read_products(category_id=0):
+    with open(os.path.join(app.root_path,"data/products.json"), encoding="utf-8") as f:
+        products = json.load(f)
+
+    if category_id > 0:
+        products = [p for p in products if p["category_id"] == category_id]
+
+    return products
+
+
+
+
